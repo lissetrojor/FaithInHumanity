74de33e6290fc7a95139734fe2a6369eb133fab8
Peyton Carter <pecarter@davidson.edu>
2020-07-29 10:40:50 -0400

    spelling fix

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function gpButton(){
   window.location= 'goodPeople.html'
   console.log('redirects to Good People')
-  }
-
+}
 function motivateButton(){
   window.location= 'motivation.html'
   console.log('redirects to Motivation')
-   }
+}
 function sjButton(){
 window.location= 'socialJustice.html'
 console.log('redirects to Social Justice')
@@ -14,9 +13,4 @@ console.log('redirects to Social Justice')
 function cuteButton(){
   window.location= 'animals.html'
   console.log('redirects to Cute Animals')
-  }
-
-
-
-// let navButtons = document.querySelector("#people-good");
-// nav.Buttons.
+}
