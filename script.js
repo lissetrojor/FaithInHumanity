0666c2f7ff2541bffb8e8c6e339e3537f215490d
Peyton Carter <pecarter@davidson.edu>
2020-07-28 23:00:57 -0400

    buttons working!

---
+++ script.js
@@ -0,0 +1,22 @@
+function gpButton(){
+  window.location= 'goodPeople.html'
+  console.log('redirects to Good People')
+  }
+
+function motivateButton(){
+  window.location= 'motivation.html'
+  console.log('redirects to Motivation')
+   }
+function sjButton(){
+window.location= 'socialJustice.html'
+console.log('redirects to Social Justice')
+}
+function cuteButton(){
+  window.location= 'animals.html'
+  console.log('redirects to Cute Animals')
+  }
+
+
+
+// let navButtons = document.querySelector("#people-good");
+// nav.Buttons.
