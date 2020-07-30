4f4ddb5b192819c8fb0e28d38c6b9cd63a4ec959
Lisset Rojo Ramirez <lrojoramirez@agnesscott.edu>
2020-07-30 00:53:52 -0400

    good people flex columns

--- goodPeople.js
+++ goodPeople.js
@@ -1,13 +1,25 @@
-fucntion splitScroll(){
-  const controller = new ScrollMagic.Controller();
+// function scrollAppear(){
+//   var ptext = document.querySelector(p);
+//   var introPosition = ptext.getBoundingClientRect().top;
+//   var screenPosition = window.innerHeight / 2;
 
-  new ScrollMagic.Scene({
-    duration: 200%,
-    triggerElement: '.titletext',
-    triggerHook: 0
-  })
-  .setPin('.titletext')
-  .addIndicators()
-  .addTo(controller);
-}
-splitScroll();
+//   if(introPosition < screenPosition){
+//     ptext.classList.add('ptext-appear');
+//   }
+// }
+
+// window.addEventListener('scroll', scrollAppear);
+
+// fucntion splitScroll(){
+//   const controller = new ScrollMagic.Controller();
+
+//   new ScrollMagic.Scene({
+//     duration: 200%,
+//     triggerElement: '.titletext',
+//     triggerHook: 0
+//   })
+//   .setPin('.titletext')
+//   .addIndicators()
+//   .addTo(controller);
+// }
+// splitScroll();
