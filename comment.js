8f43d537d394c0bba3ccd759cfd4bba9e2c00b1f
Peyton Carter <pecarter@davidson.edu>
2020-07-30 11:49:25 -0400

    comment pages

--- comment.js
+++ comment.js
@@ -3,7 +3,9 @@ let suggest = document.querySelector('#words');
 document.querySelector("#words").innerHTML = document.myform.name.value;
 
 suggest.style.color = "#292F36";
-
+suggest.style.borderBottom = "solid #292F36 2px";
+suggest.style.borderTop = "solid #292F36 2px";
+suggest.style.padding = "5px";
 
 console.log('sumbitted new entry');
 }
